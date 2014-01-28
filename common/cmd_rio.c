a30a287632e65827de9c19b17232d4a8782bbfa7
Aurelien Jacquiot <a-jacquiot@ti.com>
2014-01-28 13:25:31 +0100

    RapidIO: add support for KeyStone 2 rapidio boot
     - manage secure kernel monitor
     - add support for 'riosize', 'rioports', 'riopathmode' and 'riobaudrate'
       optional environment variables
    
    Signed-off-by: Aurelien Jacquiot <a-jacquiot@ti.com>

--- common/cmd_rio.c
+++ common/cmd_rio.c
@@ -22,13 +22,16 @@
  */
 
 /*
- * Boot support
+ * RapidIO boot support
  */
 #include <common.h>
 #include <command.h>
 #include <asm/io.h>
 
-extern void *keystone_rio_init(int riohdid);
+extern int do_mon_install(cmd_tbl_t *cmdtp, int flag, int argc,
+			  char * const argv[]);
+
+extern void *keystone_rio_init(int riohdid, int riosize, u32 rioports, int riopmode, int riobaudrate);
 
 static void *rio_handle;
 
@@ -77,13 +80,14 @@ static inline int abort_rioboot(int bootdelay)
 static int
 rioboot(cmd_tbl_t *cmdtp, int argc, char * const argv[])
 {
-	u32 kern_loaded_addr, fdt_loaded_addr;
-	char kaddr_buff[12], taddr_buff[12];
-	u32 old_val_1, old_val_2;
+	u32 kern_loaded_addr, fdt_loaded_addr, skern_loaded_addr;
+	char kaddr_buff[12], taddr_buff[12], saddr_buff[12];
+	u32 old_val_1, old_val_2, old_val_3;
 	int abort = 0, bootdelay;
 	cmd_tbl_t *bm_cmdtp;
 	char *bm_argv[4];
-	int riohdid;
+	int riohdid, riosize, riobaudrate, riopmode;
+	u32 rioports;
 	ulong addr;
 	char *s;
 	char *end;
@@ -93,7 +97,35 @@ rioboot(cmd_tbl_t *cmdtp, int argc, char * const argv[])
 	if (s != NULL)
 		riohdid = simple_strtol(s, NULL, 10);
 	else
-		riohdid = -1;
+		riohdid = -1; /* none */
+
+	/* dev-id size */
+	s = getenv("riosize");
+	if (s != NULL)
+		riosize = simple_strtol(s, NULL, 10);
+	else
+		riosize = 0; /* small Ids */
+
+	/* port bitmask */
+	s = getenv("rioports");
+	if (s != NULL)
+		rioports = simple_strtol(s, NULL, 10);
+	else
+		rioports = 0x1; /* port 0 */
+
+	/* port path mode */
+	s = getenv("riopathmode");
+	if (s != NULL)
+		riopmode = simple_strtol(s, NULL, 10);
+	else
+		riopmode = 0x0; /* 4 ports in 1x */
+
+	/* baudrate (K2 only) */
+	s = getenv("riobaudrate");
+	if (s != NULL)
+		riobaudrate = simple_strtol(s, NULL, 10);
+	else
+		riobaudrate = 1; /* 2.5Gbps */
 
 	switch (argc) {
 	case 1:
@@ -115,19 +147,26 @@ rioboot(cmd_tbl_t *cmdtp, int argc, char * const argv[])
 
 	printf("rioboot: waiting for link up ...\n");
 
-	rio_handle = keystone_rio_init(riohdid);
+	rio_handle = keystone_rio_init(riohdid,
+				       riosize,
+				       rioports,
+				       riopmode,
+				       riobaudrate);
 	if (!rio_handle)
 		return 0;
 
-	/* Wait for DIO write image to finish
-	   and get the addresses */
-
+	/* 
+	 * Wait for DIO write image to finish
+	 * and get the addresses
+	 */
 	printf("rioboot: waiting for image download ...\n");
 
 	old_val_1 = readl(0x80000000);
 	writel(0, 0x80000000);
 	old_val_2 = readl(0x80000004);
 	writel(0, 0x80000004);
+	old_val_3 = readl(0x80000008);
+	writel(0, 0x80000008);
 
 	/* kernel load address */
 	while ((kern_loaded_addr = readl(0x80000000)) == 0)
@@ -139,11 +178,15 @@ rioboot(cmd_tbl_t *cmdtp, int argc, char * const argv[])
 		udelay(1000);
 	writel(old_val_2, 0x80000004);
 
+	skern_loaded_addr = readl(0x80000008);
+	writel(old_val_3, 0x80000008);
+
 	sprintf(kaddr_buff, "0x%08x", kern_loaded_addr);
 	sprintf(taddr_buff, "0x%08x", fdt_loaded_addr);
+	sprintf(saddr_buff, "0x%08x", skern_loaded_addr);
 
-	printf("rioboot: image download done: kern@%s fdt@%s\n",
-		kaddr_buff, taddr_buff);
+	printf("rioboot: image download done: kern@%s fdt@%s skern@%s\n",
+	       kaddr_buff, taddr_buff, saddr_buff);
 
 	s = getenv("bootdelay");
 	bootdelay = s ? (int)simple_strtol(s, NULL, 10) : CONFIG_BOOTDELAY;
@@ -153,6 +196,16 @@ rioboot(cmd_tbl_t *cmdtp, int argc, char * const argv[])
 	keystone_rio_shutdown(rio_handle);
 
 	if (!abort) {
+		if (skern_loaded_addr) {
+			
+			/* load secure kernel monitor */
+			bm_cmdtp = find_cmd("mon_install");
+			
+			bm_argv[0] = "mon_install";
+			bm_argv[1] = &(saddr_buff[0]);
+			do_mon_install(bm_cmdtp, 0, 2, bm_argv);
+		}
+		/* boot Linux */
 		bm_cmdtp = find_cmd("bootm");
 
 		bm_argv[0] = "bootm";
@@ -175,4 +228,3 @@ U_BOOT_CMD(
 	"boot image via rio",
 	""
 );
-
